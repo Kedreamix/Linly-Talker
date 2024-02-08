af84caf9e110db33a47fc1fd19e378960723bcbe
Kedreamix <kedreamix@gmail.com>
2024-02-08 09:15:55 +0000

    🚀🚀🚀加入Linly Talker WebUI

--- webui.py
+++ webui.py
@@ -30,14 +30,14 @@ def get_title(title = 'Linly 智能对话系统 (Linly-Talker)'):
     return description
 
 # 默认text的Example
-examples =  ['应对压力最有效的方法是什么？',
-            '如何进行时间管理？',
-            '为什么有些人选择使用纸质地图或寻求方向，而不是依赖GPS设备或智能手机应用程序？',
-            '近日，苹果公司起诉高通公司，状告其未按照相关合约进行合作，高通方面尚未回应。这句话中“其”指的是谁？',
-            '三年级同学种树80颗，四、五年级种的棵树比三年级种的2倍多14棵，三个年级共种树多少棵?',
-            '撰写一篇交响乐音乐会评论，讨论乐团的表演和观众的整体体验。',
-            '翻译成中文：Luck is a dividend of sweat. The more you sweat, the luckier you get.',
-            ]
+examples =  [
+    ['应对压力最有效的方法是什么？', '女性角色', 'SadTalker', 'zh-CN-XiaoxiaoNeural'],
+    ['如何进行时间管理？','男性角色', 'SadTalker', 'zh-CN-YunyangNeural'],
+    ['为什么有些人选择使用纸质地图或寻求方向，而不是依赖GPS设备或智能手机应用程序？','女性角色', 'SadTalker', 'zh-HK-HiuMaanNeural'],
+    ['近日，苹果公司起诉高通公司，状告其未按照相关合约进行合作，高通方面尚未回应。这句话中“其”指的是谁？', '男性角色', 'SadTalker', 'zh-TW-YunJheNeural'],
+    ['撰写一篇交响乐音乐会评论，讨论乐团的表演和观众的整体体验。', '男性角色', 'Wav2Lip', 'zh-CN-YunyangNeural'],
+    ['翻译成中文：Luck is a dividend of sweat. The more you sweat, the luckier you get.', '女性角色', 'SadTalker', 'zh-CN-XiaoxiaoNeural'],
+    ]
 
 # 设置默认system
 default_system = '你是一个很有帮助的助手'
@@ -63,21 +63,59 @@ def Asr(audio):
         question = convert(question, 'zh-cn')
     except Exception as e:
         print("ASR Error: ", e)
-        question = 'Gradio 的麦克风有时候可能音频还未传入，请重试一下'
+        question = 'Gradio存在一些bug，麦克风模式有时候可能音频还未传入，请重新点击一下语音识别即可'
+        gr.Warning(question)
     return question
 
 @calculate_time
-def LLM_response(question, voice = 'zh-CN-XiaoxiaoNeural', rate = 0, volume = 0, pitch = 0):
+def LLM_response(question_audio, question, voice = 'zh-CN-XiaoxiaoNeural', rate = 0, volume = 0, pitch = 0):
     answer = llm.generate(question)
     print(answer)
-    try:
-        tts.predict(answer, voice, rate, volume, pitch , 'answer.wav', 'answer.vtt')
-    except:
-        os.system(f'edge-tts --text "{answer}" --voice {voice} --write-media answer.wav')
-    return 'answer.wav', 'answer.vtt', answer
+    if voice in tts.SUPPORTED_VOICE:
+        try:
+            tts.predict(answer, voice, rate, volume, pitch , 'answer.wav', 'answer.vtt')
+        except:
+            os.system(f'edge-tts --text "{answer}" --voice {voice} --write-media answer.wav')
+        return 'answer.wav', 'answer.vtt', answer
+    elif voice == "克隆烟嗓音":
+        try:
+            gpt_path = "../GPT-SoVITS/GPT_weights/yansang-e15.ckpt"
+            sovits_path = "../GPT-SoVITS/SoVITS_weights/yansang_e16_s144.pth"
+            vits.load_model(gpt_path, sovits_path)
+            vits.predict(ref_wav_path = "examples/slicer_opt/vocal_output.wav_10.wav_0000846400_0000957760.wav",
+                        prompt_text = "你为什么要一次一次的伤我的心啊？",
+                        prompt_language = "中文",
+                        text = answer,
+                        text_language = "中英混合",
+                        how_to_cut = "按标点符号切",
+                        save_path = 'answer.wav')
+            return 'answer.wav', None, answer
+        except Exception as e:
+            gr.Error("无克隆环境或者无克隆模型权重，无法克隆声音", e)
+            return None, None, None
+    elif voice == "克隆声音":
+        try:
+            if question_audio is None:
+                gr.Error("无声音输入，无法克隆声音")
+                # print("无声音输入，无法克隆声音")
+                return None, None, None
+            gpt_path = "GPT_SoVITS/pretrained_models/s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt"
+            sovits_path = "GPT_SoVITS/pretrained_models/s2G488k.pth"
+            vits.load_model(gpt_path, sovits_path)
+            vits.predict(ref_wav_path = question_audio, 
+                        prompt_text = question,
+                        prompt_language = "中文",
+                        text = answer,
+                        text_language = "中英混合",
+                        how_to_cut = "凑四句一切",
+                        save_path = 'answer.wav')
+            return 'answer.wav', None, answer
+        except Exception as e:
+            gr.Error("无克隆环境或者无克隆模型权重，无法克隆声音", e)
+            return None, None, None
 
 @calculate_time
-def Talker_response(method = 'SadTalker', text = '', voice = 'zh-CN-XiaoxiaoNeural', rate = 0, volume = 100, pitch = 0, batch_size = 2, character = '女性角色'):
+def Talker_response(question_audio = None, method = 'SadTalker', text = '', voice = 'zh-CN-XiaoxiaoNeural', rate = 0, volume = 100, pitch = 0, batch_size = 2, character = '女性角色'):
     if character == '女性角色':
         # 女性角色
         source_image, pic_path = r'girl.png', r"girl.png"
@@ -93,9 +131,12 @@ def Talker_response(method = 'SadTalker', text = '', voice = 'zh-CN-XiaoxiaoNeur
         first_coeff_path = "./inputs/first_frame_dir_boy/boy.mat"
         crop_info = ((876, 747), (0, 0, 886, 838), [10.382158280494476, 0, 886, 747.7078990925525])
         default_voice = 'zh-CN-YunyangNeural'
-    voice = default_voice if voice not in tts.SUPPORTED_VOICE else voice
+    else:
+        gr.Error('未知角色')
+        return None
+    voice = default_voice if voice not in tts.SUPPORTED_VOICE+["克隆烟嗓音", "克隆声音"] else voice
     print(voice, character)
-    driven_audio, driven_vtt, _ = LLM_response(text, voice, rate, volume, pitch)
+    driven_audio, driven_vtt, _ = LLM_response(question_audio, text, voice, rate, volume, pitch)
     pose_style = random.randint(0, 45)
     if method == 'SadTalker':
         video = talker.test(pic_path,
@@ -194,10 +235,11 @@ def clear_session():
     llm.clear_history()
     return '', []
 
-def voice_setting():
+def voice_setting(suport_voice):
     with gr.Accordion("Advanced Settings(高级设置语音参数) ", open=False):
-        voice = gr.Dropdown(tts.SUPPORTED_VOICE,  
-                            label="声音选择 Voice")
+        voice = gr.Dropdown(suport_voice,  
+                            label="声音选择 Voice", 
+                            value = "克隆声音" if '克隆声音' in suport_voice else None)
         rate = gr.Slider(minimum=-100,
                             maximum=100,
                             value=0,
@@ -224,7 +266,7 @@ def voice_setting():
     return  voice, rate, volume, pitch, batch_size, character, method
 
 @calculate_time
-def Talker_response_img(method, text, voice, rate, volume, pitch, source_image,
+def Talker_response_img(question_audio, method, text, voice, rate, volume, pitch, source_image,
                     preprocess_type, 
                     is_still_mode,
                     enhancer,
@@ -235,8 +277,7 @@ def Talker_response_img(method, text, voice, rate, volume, pitch, source_image,
                     exp_weight,
                     blink_every,
                     fps):
-    voice = 'zh-CN-XiaoxiaoNeural' if voice not in tts.SUPPORTED_VOICE else voice
-    driven_audio, driven_vtt, _ = LLM_response(text, voice, rate, volume, pitch)
+    driven_audio, driven_vtt, _ = LLM_response(question_audio, text, voice, rate, volume, pitch)
     if method == 'SadTalker':
         video = talker.test2(source_image,
                         driven_audio,
@@ -274,7 +315,7 @@ def app():
                         with gr.Column(variant='panel'):
                             question_audio = gr.Audio(sources=['microphone','upload'], type="filepath", label = '语音对话')
                             input_text = gr.Textbox(label="Input Text", lines=3)
-                            voice, rate, volume, pitch, batch_size, character, method = voice_setting()
+                            voice, rate, volume, pitch, batch_size, character, method = voice_setting(tts.SUPPORTED_VOICE)
                             asr_text = gr.Button('语音识别（语音对话后点击）')
                             asr_text.click(fn=Asr,inputs=[question_audio],outputs=[input_text])
                         
@@ -283,18 +324,16 @@ def app():
                     with gr.TabItem('数字人问答'):
                         gen_video = gr.Video(label="生成视频", format="mp4", scale=1, autoplay=False)
                 video_button = gr.Button("提交视频生成", variant='primary')
-            video_button.click(fn=Talker_response,inputs=[method, input_text,voice, rate, volume, pitch, batch_size, character],outputs=[gen_video])
+            video_button.click(fn=Talker_response,inputs=[question_audio, method, input_text,voice, rate, volume, pitch, batch_size, character],outputs=[gen_video])
 
         with gr.Row():
             with gr.Column(variant='panel'):
-                    gr.Markdown("## Text Examples")
-                    gr.Examples(
-                        examples = examples,
-                        fn = Talker_response,
-                        inputs = [input_text],
-                        outputs=[gen_video],
-                        # cache_examples = True,
-                    )
+                gr.Markdown("## Test Examples")
+                gr.Examples(
+                    examples = examples,
+                    fn = Talker_response,
+                    inputs = [input_text, character, method, voice],
+                )
     return inference
 
 def app_multi():
@@ -302,7 +341,7 @@ def app_multi():
         gr.HTML(get_title("Linly 智能对话系统 (Linly-Talker) 多轮GPT对话"))
         with gr.Row():
             with gr.Column():
-                voice, rate, volume, pitch, batch_size, character, method = voice_setting()
+                voice, rate, volume, pitch, batch_size, character, method = voice_setting(tts.SUPPORTED_VOICE)
                 video = gr.Video(label = '数字人问答', scale = 0.5)
                 video_button = gr.Button("🎬 生成数字人视频（对话后）", variant = 'primary')
             
@@ -341,14 +380,12 @@ def app_multi():
             video_button.click(fn = human_respone, inputs = [chatbot, voice, rate, volume, pitch, batch_size, character], outputs = [video])
             
         with gr.Row(variant='panel'):
-            with gr.Column():
-                gr.Markdown("## Text Examples")
+            with gr.Column(variant='panel'):
+                gr.Markdown("## Test Examples")
                 gr.Examples(
                     examples = examples,
-                    inputs = [msg],
-                    # fn = Talker_response,
-                    # outputs=[gen_video],
-                    # cache_examples = True,
+                    fn = Talker_response,
+                    inputs = [msg, character, method, voice],
                 )
     return inference
 
@@ -527,26 +564,24 @@ def app_vits():
                         with gr.Column(variant='panel'):
                             question_audio = gr.Audio(sources=['microphone','upload'], type="filepath", label = '语音对话')
                             input_text = gr.Textbox(label="Input Text", lines=3)
-                            voice, rate, volume, pitch, batch_size = voice_setting()
+                            voice, rate, volume, pitch, batch_size, character, method = voice_setting(["克隆声音", "克隆烟嗓音"] + tts.SUPPORTED_VOICE)
                             asr_text = gr.Button('语音识别（语音对话后点击）')
                             asr_text.click(fn=Asr,inputs=[question_audio],outputs=[input_text])
             with gr.Column(variant='panel'): 
                 with gr.Tabs():
                     with gr.TabItem('数字人问答'):
-                        gen_video = gr.Video(label="Generated video", format="mp4", scale=1, autoplay=True)
+                        gen_video = gr.Video(label="Generated video", format="mp4", scale=1, autoplay=False)
                 video_button = gr.Button("提交", variant='primary')
-            video_button.click(fn=Talker_response,inputs=[input_text,voice, rate, volume, pitch, batch_size],outputs=[gen_video])
+            video_button.click(fn=Talker_response,inputs=[question_audio, method, input_text, voice, rate, volume, pitch, batch_size, character],outputs=[gen_video])
 
         with gr.Row():
             with gr.Column(variant='panel'):
-                    gr.Markdown("## Text Examples")
-                    gr.Examples(
-                        examples = examples,
-                        fn = Talker_response,
-                        inputs = [input_text],
-                        outputs=[gen_video],
-                        # cache_examples = True,
-                    )
+                gr.Markdown("## Test Examples")
+                gr.Examples(
+                    examples = [["如何应对压力", "男性角色", "SadTalker", "克隆烟嗓音"], ["北京有什么好玩的地方", "男性角色", "SadTalker", "克隆声音"]] + examples,
+                    fn = Talker_response,
+                    inputs = [input_text, character, method, voice],
+                )
     return inference
     
 if __name__ == "__main__":
@@ -554,26 +589,44 @@ if __name__ == "__main__":
     # llm = LLM(mode='offline').init_model('Gemini', 'gemini-pro', api_key = "your api key")
     # llm = LLM(mode='offline').init_model('Qwen', 'Qwen/Qwen-1_8B-Chat')
     llm = LLM(mode='offline').init_model('Qwen', 'Qwen/Qwen-1_8B-Chat')
-    talker = SadTalker(lazy_load=True)
+    try:
+        talker = SadTalker(lazy_load=True)
+    except Exception as e:
+        print("SadTalker Error: ", e)
+        # print("如果使用SadTalker，请先下载SadTalker模型")
+        gr.Warning("如果使用SadTalker，请先下载SadTalker模型")
     try:
         from TFG import Wav2Lip
-        wav2lip = Wav2Lip()
+        wav2lip = Wav2Lip("checkpoints/wav2lip_gan.pth")
     except Exception as e:
         print("Wav2Lip Error: ", e)
         print("如果使用Wav2Lip，请先下载Wav2Lip模型")
-    asr = WhisperASR('base')
+    try:
+        from VITS import GPT_SoVITS
+        vits = GPT_SoVITS()
+    except Exception as e:
+        print("GPT-SoVITS Error: ", e)
+        print("如果使用VITS，请先下载GPT-SoVITS模型和安装环境")
+    try:
+        from ASR import FunASR
+        asr = FunASR()
+    except Exception as e:
+        print("ASR Error: ", e)
+        print("如果使用FunASR，请先下载FunASR模型和安装环境")
+        asr = WhisperASR('base')
     tts = EdgeTTS()
     gr.close_all()
     demo_app = app()
     demo_img = app_img()
     demo_multi = app_multi()
-    # demo_vits = app_vits()
-    demo = gr.TabbedInterface(interface_list = [demo_app, demo_img, demo_multi, ], 
-                              tab_names = ["文本/语音对话", "任意图片对话", "多轮GPT对话", ],
+    demo_vits = app_vits()
+    demo = gr.TabbedInterface(interface_list = [demo_app, demo_img, demo_multi, demo_vits], 
+                              tab_names = ["文本/语音对话", "任意图片对话", "多轮GPT对话", "语音克隆数字人对话"],
                               title = "Linly-Talker WebUI")
     demo.launch(server_name="127.0.0.1", # 本地端口localhost:127.0.0.1 全局端口转发:"0.0.0.0"
                 server_port=port,
                 ssl_certfile=ssl_certfile,
                 ssl_keyfile=ssl_keyfile,
                 ssl_verify=False,
-                debug=True)
+                debug=True,
+                )
