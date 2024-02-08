9a3f66feda6f0c70ad5869eb279b91585929360b
Kedreamix <kedreamix@gmail.com>
2024-02-08 06:01:48 +0000

    update VITS and add XTTS

--- VITS/__init__.py
+++ VITS/__init__.py
@@ -1,1 +1,9 @@
-from .GPT_SoVITS import GPT_SoVITS
+try:
+    from .GPT_SoVITS import GPT_SoVITS
+except:
+    print("使用GPT-SoVITS语音克隆前需要安装对应的环境，请执行 pip install -r VITS/requirements_vits.txt")
+    
+try:
+    from .XTTS import XTTS
+except:
+    print("使用XTTS语音克隆前需要安装对应的环境，请执行 pip install -r VITS/requirements_xtts.txt")
