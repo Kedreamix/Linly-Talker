cd687d4a21610ccda3b9623b0eab851394cc0bb1
Kedreamix <kedreamix@gmail.com>
2024-04-22 14:12:33 +0800

    update paddlespeech TTS

--- TTS/__init__.py
+++ TTS/__init__.py
@@ -1,1 +1,8 @@
 from .EdgeTTS import EdgeTTS
+
+try:
+    from .PaddleTTS import PaddleTTS
+except Exception as e:
+    print("PaddleTTS Error: ", e)
+    print("如果使用PaddleTTS，请先安装PaddleTTS环境")
+    print("pip install -r requirements_paddle.txt")
